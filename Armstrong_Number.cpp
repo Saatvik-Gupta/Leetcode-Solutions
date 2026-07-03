37efbb26819802044516c8409336ba17203d9ff2
Saatvik <shobhita65403@gmail.com>
2026-07-03 15:54:49 +0530

    Leetcode Soln

---
+++ Armstrong_Number.cpp
@@ -0,0 +1,51 @@
+// An Armstrong Number is a number whose sum of the power of its digits it contains is equal to the original number
+/*ex:
+153 is an Armstrong Number
+digits count=3
+1^3 + 5^3 + 3^3=>1+125+27==>153
+*/
+
+#include<iostream>
+using namespace std;
+
+int digit(int num){
+    // count no. of digits
+    int count=0;
+    while(num!=0){
+        count++;
+        num/=10;
+    }
+    return count;
+}
+
+bool check_Armstrong(int num){
+    int temp=num;
+    int sum=0;
+    int c=digit(num);
+
+    while(temp!=0){
+        int digit=temp%10;
+        int p=1;
+        for(int i=1;i<=c;i++){
+            p *=digit;
+        }
+        sum+=p;
+        temp/=10;
+    }
+
+    return sum==num;
+}
+
+int main(){
+    int num;
+    cout<<"Enter the Number to be checked as Armstrong:";
+    cin>>num;
+
+    if(check_Armstrong(num)){
+        cout<<num<<" is an Armstrong Number"<<endl;
+    }
+    else{
+        cout<<"Not an Arnstrong Number"<<endl;
+    }
+    return 0;
+}
---
+++ Happy_Number.cpp
@@ -0,0 +1,55 @@
+// A happy number is a number whose repeatedly sum of the squares of digits finally leads to 1
+// ex:- 7 is a Happy Number as
+/*
+7^2= 49
+4^2 + 9^2=>16 + 81=>97
+9^2 + 7^2=>81 + 49=>130
+1^2 + 3^2 + 0^2=>10
+1^2 + 0^2=1 Thus a Happy Number
+*/
+
+#include<iostream>
+using namespace std;
+#include<unordered_set>
+
+int sum_digits(int num){
+    int sum=0;
+
+    while(num!=0){
+        int digit=num%10;
+        sum+= digit * digit;
+        num/=10;
+    }
+
+    return sum;
+}
+
+bool check_happynum(int num){
+    unordered_set<int>s;
+
+    while(num!=1 && s.find(num)==s.end()){
+
+        s.insert(num);
+        num=sum_digits(num);
+    }
+
+    return num==1;
+
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter the Number to be checked as a Happy Number:";
+    cin>>n;
+    
+    if(check_happynum(n)){
+        cout<<n<<" is a Happy Number"<<endl;
+    }
+
+    else{
+        cout<<"Not a Happy Number"<<endl;
+    }
+
+    return 0;
+}
