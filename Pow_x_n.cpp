1db36cee4c83d81ed4a5a2cbf708092a7acab5f4
Saatvik <shobhita65403@gmail.com>
2026-07-03 18:10:05 +0530

    Leetcode Soln

---
+++ Pow_x_n.cpp
@@ -0,0 +1,44 @@
+// To calculate (base)^exponent using binary exponentiation x^n--->Pow(x,n)
+
+#include<iostream>
+using namespace std;
+
+double Pow(double x,int n){
+
+    long long int binum=n;
+    double ans=1.0;
+
+    if(n==0) return 1;
+    if(n==1) return x;
+    if(x==0) return 0;
+
+    if(binum<0){
+        binum=-binum;
+        x=1/x;
+    }
+
+    while(binum!=0){
+
+        if(binum % 2==1){
+            ans*=x;
+        }
+        x=x*x;
+
+        binum=binum/2;
+    }
+    return ans;
+}
+
+int main(){
+
+    double base;
+    int expo;
+    cout<<"Enter the base:";
+    cin>>base;
+    cout<<"Enter the exponent:";
+    cin>>expo;
+
+    cout<<base<<" ^ "<<expo<<" is: "<<Pow(base,expo)<<endl;
+
+    return 0;
+}
---
+++ palindrome_int.cpp
@@ -0,0 +1,44 @@
+// Palindrome numbers are those which can we read same from both the ends ex-121
+
+#include<iostream>
+using namespace std;
+
+int reverse(int num){
+
+    int rev=0;
+    if(num<0){
+        return 0; // palindrome not defined for -ve numbers
+    }
+    while(num!=0){
+        int digit=num%10;
+
+        if(rev> INT32_MAX || rev==INT32_MAX && digit>7){//  2^31-1 last digit 7
+            return 0;
+        }
+
+        if(rev<INT32_MIN || rev==INT32_MIN && digit<-8){//  -2^31 last digit 8
+            return 0;
+        }
+        rev= rev*10 + digit;
+        num=num/10;
+    }
+
+    return rev;
+}
+
+int main(){
+
+    int num;
+    cout<<"Enter the number to be checked as Palindrome:";
+    cin>>num;
+
+    int rev=reverse(num);
+
+    if(num==rev){
+        cout<<num<<" is a Palindrome number"<<endl;
+    }
+    else{
+        cout<<num<<" is not a Palindrome number"<<endl;
+    }
+    return 0;
+}
