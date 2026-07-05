e06aa998ca0f6cecc71cbcaa12a0a2251adf3ff5
Saatvik <shobhita65403@gmail.com>
2026-07-05 16:35:30 +0530

    Sorting Algorithms

--- sorting.cpp
+++ sorting.cpp
@@ -32,8 +32,63 @@ void bubblesort(vector<int>nums){
 
 }
 
+void SelectionSort(vector<int>nums){ // minimum element logic
+    int n=nums.size();
+
+    cout<<"Elements before sorting:"<<endl;
+    for(int val: nums){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+    for(int i=0; i<n-1; i++){
+        int min=i;
+        for(int j= i+1; j<n; j++){
+            if(nums[j]<nums[min]){
+                min=j;
+            }
+        }
+        swap(nums[i], nums[min]);
+    }
+
+    cout<<"Elements after sorting:"<<endl;
+    for(int val: nums){
+        cout<<val<<" ";
+    }
+
+}
+
+void Insertionsort(vector<int>nums){ // like playing cards
+     int n=nums.size();
+
+    cout<<"Elements before sorting:"<<endl;
+    for(int val: nums){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+    for(int i=1; i<n; i++){
+        int current=nums[i];
+        int prev=i-1;
+        while(prev>=0 && nums[prev]>current){
+            nums[prev+1]=nums[prev];
+            prev--;
+        }
+        nums[prev+1]=current;
+    }
+
+    cout<<"Elements after sorting:"<<endl;
+    for(int val: nums){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+}
+
 int main(){
     vector<int>sort={ 1,6,3,9,7,4};
-    bubblesort(sort);
+    //bubblesort(sort);
+    //SelectionSort(sort);
+    Insertionsort(sort);
     return 0;
 }
