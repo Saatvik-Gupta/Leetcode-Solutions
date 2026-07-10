5bac7ef12479e653db1771b9d844670e73beafdf
Saatvik <shobhita65403@gmail.com>
2026-07-10 12:31:02 +0530

    Leetcode Problem Number : 240

--- Search_2D_Matrix2.cpp
+++ Search_2D_Matrix2.cpp
@@ -8,5 +8,55 @@ Example 1:
 Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
 Output: true
 
+Time Complexity : O(m+n)
+
 LEETCODE Problem Number---240
 */
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+bool isSearch(vector<vector<int>> &arr, int row, int col, int target){
+
+    int r=0;
+    int c=col-1;
+
+    while( c>=0 && r< row){
+
+        if(target== arr[r][c]){
+            return true;
+        }
+        else if(target< arr[r][c]) // left search
+        {
+            c--;
+        }
+        else r++;
+    }
+    return false;
+}
+
+int main(){
+    vector<vector<int>>arr={
+        {1,4,7,11,15},
+        {2,5,8,12,19},
+        {3,6,9,16,22},
+        {10,13,14,17,24},
+        {18,21,23,26,30}
+    };
+
+    int m=arr.size();
+    int n=arr[0].size();
+
+    int target=5;
+    bool store=isSearch(arr,m,n,target);
+
+    if(store){
+        cout<<"Element exists in the given array"<<endl;
+    }
+    else{
+        cout<<"Element not found !"<<endl;
+
+    }
+        return 0;
+}
