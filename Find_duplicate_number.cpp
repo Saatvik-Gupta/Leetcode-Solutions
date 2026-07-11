6fa3266f6f3a5f06a44cddd6c1a4122a0a84dc6c
Saatvik <shobhita65403@gmail.com>
2026-07-11 09:46:58 +0530

    Leetcode Problem Number : 287

--- Find_duplicate_number.cpp
+++ Find_duplicate_number.cpp
@@ -33,11 +33,42 @@ int Repeating_num(vector<int> &arr){ // Time Complexity:O(n), Space Complexity: 
 return -1;
 }
 
+int Optimal_Repeating_find(vector<int> &arr){ // Time Complexity:O(n), Space Complexity: O(1)
+
+    // slow-fast approach where we assume our array to be a linked list with node as index and next node as element
+
+    int slow=arr[0];
+    int fast=arr[0];
+    int n=arr.size();
+
+    do{
+        slow=arr[slow]; //+1
+        fast= arr[arr[fast]]; //+2
+    } while( slow!=fast);
+
+    slow=arr[0]; // reset slow
+
+    while(slow!=fast){
+        slow=arr[slow]; //+1
+        fast=arr[fast]; //+1
+    }
+
+    // now slow and fast at same element which is the repeated one slow=fast return any
+
+    return slow;
+
+}
+
 int main(){
     vector<int>arr={1,3,2,4,2,2};
 
-    int store=Repeating_num(arr);
-    cout<<"Repeating element in array is :"<<store<<endl;
+    // int store=Repeating_num(arr); // Space Complexity is O(n) but question demands O(1)
+    // cout<<"Repeating element in array is :"<<store<<endl;
+
+    // use this
+    int store=Optimal_Repeating_find(arr);
+     cout<<"Repeating element in array is :"<<store<<endl;
+
     return 0;
 
 }
