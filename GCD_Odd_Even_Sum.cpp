bd54cf73cc81cfcd9e6111cb5f6bef0ded75257a
Saatvik <shobhita65403@gmail.com>
2026-07-15 06:26:34 +0530

    Leetcode Problem Number : 3658

--- GCD_Odd_Even_Sum.cpp
+++ GCD_Odd_Even_Sum.cpp
@@ -24,3 +24,42 @@ Hence, GCD(sumOdd, sumEven) = GCD(25, 30) = 5.
 LEETCODE Problem Number---3658
 */
 
+#include<iostream>
+using namespace std;
+
+int GCD_Odd_Even_Number(int n){
+
+    int sum_even=0;
+    int sum_odd=0;
+
+    // Calculate even sum
+    for(int i=1;i<=n;i++){
+        sum_even+= (i*2); // --2i--2,4,6,8,10......n
+    }
+
+    // Calculate odd sum
+    for(int i=1;i<=n;i++){
+        sum_odd+= (2*i-1); // 1,3,5,7....n
+    }
+
+    // Calculating Gcd by eulers algorithm
+
+    while(sum_even>0 && sum_odd>0){
+        if(sum_even>sum_odd) sum_even=sum_even%sum_odd;
+
+        else sum_odd=sum_odd%sum_even;
+    }
+
+    if(sum_even==0) return sum_odd;
+    else return sum_even;
+
+}
+
+int main(){
+    int num;
+    cout<<"Enter the Range to find GCD of Even and odd Number's Sum in between:";
+    cin>>num;
+    int store=GCD_Odd_Even_Number(num);
+    cout<<"GCD of Even and Odd Sum in range "<<num<<" is: "<<store<<endl;
+    return 0;
+}
