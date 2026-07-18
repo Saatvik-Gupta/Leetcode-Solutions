200da4f3356dd23266e31b22410c7c55f4dda683
Saatvik <shobhita65403@gmail.com>
2026-07-18 09:13:14 +0530

    Leetcode Problem Number : 78

--- Subsets_1.cpp
+++ Subsets_1.cpp
@@ -14,3 +14,41 @@ Output: [[],[0]]
 LEETCODE Problem Number---78
 */
 
+#include<iostream>
+using namespace std;
+#include<vector>
+
+void Subsets(vector<int>& nums,vector<int>& ans,int idx){
+
+    // Base condition
+    if(idx==nums.size()){
+
+    for(int val: ans){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return;
+
+    }
+
+    // inclusion
+    ans.push_back(nums[idx]);
+    Subsets(nums,ans,idx+1);
+
+    ans.pop_back(); // backtracking
+
+    // exclusion
+    Subsets(nums,ans,idx+1);
+}
+
+int main(){
+    vector<int>nums={1,2,3};
+
+    vector<int>ans;
+
+    cout<<"All Posiible subsets are:"<<endl;
+
+    Subsets(nums,ans,0);
+    return 0;
+
+}
